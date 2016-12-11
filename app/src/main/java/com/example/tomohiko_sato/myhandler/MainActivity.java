7f15ad3e864587d81299cb2a18f135b360f374be
tomohiko sato <tomohikosato1@gmail.com>
2016-12-11 12:32:01 +0900

    Looperを持ったWorkerThreadを用意し、その中でHandlerを作成

--- app/src/main/java/com/example/tomohiko_sato/myhandler/MainActivity.java
+++ app/src/main/java/com/example/tomohiko_sato/myhandler/MainActivity.java
@@ -2,60 +2,56 @@ package com.example.tomohiko_sato.myhandler;
 
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.HandlerThread;
 import android.os.Looper;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+	LooperThread looperThread;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-//		final TextView tv = (TextView) findViewById(R.id.text_view);
 
+		Button button = (Button) findViewById(R.id.button);
+		button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				looperThread.handler.sendMessage(Message.obtain(null, 345, "hogehoge"));
+			}
+		});
+
+		looperThread = new LooperThread();
+		looperThread.start();
 	}
 
-	public class LooperThread extends Thread {
-		Looper looper;
 
-		@Override
+	class LooperThread extends Thread {
+		public Handler handler;
+
 		public void run() {
 			Looper.prepare();
-			looper = Looper.myLooper();
-			synchronized (this) {
-				notifyAll();
-			}
-			Looper.loop();
-		}
 
-		public Looper getLooper() {
-			try {
-				synchronized (this) {
-					wait();
+			handler = new Handler() {
+				public void handleMessage(Message msg) {
+					Log.d("handler", msg.toString());
+					// process incoming messages here
 				}
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			return looper;
+			};
+
+			Looper.loop();
 		}
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
-		LooperThread looperThread = new LooperThread();
-		looperThread.start();
-
-		new Handler(looperThread.getLooper()).post(new Runnable() {
-			@Override
-			public void run() {
-				Toast.makeText(MainActivity.this, "fugafuga", Toast.LENGTH_SHORT).show();
-			}
-		});
-
-
 	}
 }
