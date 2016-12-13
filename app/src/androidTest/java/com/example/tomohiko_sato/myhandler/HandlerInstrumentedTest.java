f716c65e4dd17191f159ade1bc83e46c8d2c49c4
tomohiko sato <tomohikosato1@gmail.com>
2016-12-13 21:40:52 +0900

    テスト書いた

---
+++ app/src/androidTest/java/com/example/tomohiko_sato/myhandler/HandlerInstrumentedTest.java
@@ -0,0 +1,75 @@
+package com.example.tomohiko_sato.myhandler;
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.os.Looper.getMainLooper;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.timeout;
+import static org.mockito.Mockito.verify;
+
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class HandlerInstrumentedTest {
+
+	@Test
+	public void Handlerでメインスレッドに通知できる() {
+		//テストはワーカースレッドで動作しているようなのでMainLooperを指定
+		final Handler handler = new Handler(Looper.getMainLooper());
+
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				handler.post(new Runnable() {
+					@Override
+					public void run() {
+						assertEquals(Thread.currentThread(), getMainLooper().getThread());
+					}
+				});
+			}
+		}).start();
+	}
+
+	@Test
+	public void 普通に生成したThreadにはLooperが付いていないのでHandlerが使えない() {
+		final Runnable mockR = mock(Runnable.class);
+
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					new Handler().post(mockR);
+				} catch (RuntimeException e) {
+					assertEquals(e.getMessage(), "Can't create handler inside thread that has not called Looper.prepare()");
+				}
+			}
+		}).start();
+
+		verify(mockR, never()).run();
+	}
+
+
+	@Test
+	public void HandlerThreadではHandlerが使える() {
+		HandlerThread handlerThread = new HandlerThread("hoge");
+		handlerThread.start();
+		Runnable mockR = mock(Runnable.class);
+		new Handler(handlerThread.getLooper()).post(mockR);
+
+		verify(mockR, timeout(100).times(1)).run(); // 非同期にrun()が呼ばれるので少し遅延させる
+	}
+}
