ecd4fe4d3ab01a9d5d5123e25581f0ecdfd30be4
tomohiko sato <tomohikosato1@gmail.com>
2016-12-16 01:52:58 +0900

    ログを追加。Thread.sleepがあると連打した場合に微妙なので削除

--- app/src/main/java/com/example/tomohiko_sato/myhandler/MainActivity.java
+++ app/src/main/java/com/example/tomohiko_sato/myhandler/MainActivity.java
@@ -4,22 +4,16 @@ import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.Handler;
 import android.os.IBinder;
-import android.os.Looper;
-import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
 	private final String TAG = MainActivity.class.getSimpleName();
 	private WorkerThreadService service;
-	private boolean isBound;
+	private boolean isBound = false;
 
 	private final ServiceConnection conn = new ServiceConnection() {
 		@Override
@@ -63,14 +57,14 @@ public class MainActivity extends AppCompatActivity {
 	@Override
 	protected void onStart() {
 		super.onStart();
-		Log.d(TAG, "bind service");
+		Log.d(TAG, "onstart bind service");
 		bindService(new Intent(this, WorkerThreadService.class), conn, BIND_AUTO_CREATE);
 	}
 
 	@Override
 	protected void onStop() {
 		super.onStop();
-		Log.d(TAG, "unbind service");
+		Log.d(TAG, "onstop unbind service");
 		unbindService(conn);
 		isBound = false;
 	}
--- app/src/main/java/com/example/tomohiko_sato/myhandler/WorkerThreadService.java
+++ app/src/main/java/com/example/tomohiko_sato/myhandler/WorkerThreadService.java
@@ -14,38 +14,15 @@ import android.widget.Toast;
 public class WorkerThreadService extends Service {
 	private final static String TAG = WorkerThreadService.class.getSimpleName();
 	private ServiceBinder binder = new ServiceBinder();
-	private final Handler serviceHandler;
+	private Handler serviceHandler;
 
 	private final static int MSG_HELLO = 123;
 	private final static int MSG_BYE = 343;
 
-	public WorkerThreadService() {
-		HandlerThread handlerThread = new HandlerThread("WorkerThreadService");
-		handlerThread.start();
+	private final static String MSG_HELLO_OBJ = "konichiwa";
+	private final static String MSG_BYE_OBJ = "sayonara";
 
-		serviceHandler = new Handler(handlerThread.getLooper()) {
-			@Override
-			public void handleMessage(Message msg) {
-				switch (msg.what) {
-					case MSG_HELLO:
-						try {
-							Thread.sleep(1000);
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-						Toast.makeText(WorkerThreadService.this, "hello", Toast.LENGTH_SHORT).show();
-						break;
-					case MSG_BYE:
-						try {
-							Thread.sleep(5000);
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-						Toast.makeText(WorkerThreadService.this, "bye", Toast.LENGTH_SHORT).show();
-						break;
-				}
-			}
-		};
+	public WorkerThreadService() {
 	}
 
 	class ServiceBinder extends Binder {
@@ -56,14 +33,41 @@ public class WorkerThreadService extends Service {
 
 	@Override
 	public IBinder onBind(Intent intent) {
+		Log.d(TAG, "onBind");
+		HandlerThread handlerThread = new HandlerThread("WorkerThreadService");
+		handlerThread.start();
+
+		serviceHandler = new Handler(handlerThread.getLooper()) {
+			@Override
+			public void handleMessage(Message msg) {
+				Log.d(TAG, msg.toString());
+
+				String messageObj = (String) msg.obj;
+				switch (msg.what) {
+					case MSG_HELLO:
+						Toast.makeText(WorkerThreadService.this, messageObj, Toast.LENGTH_SHORT).show();
+						break;
+					case MSG_BYE:
+						Toast.makeText(WorkerThreadService.this, messageObj, Toast.LENGTH_SHORT).show();
+						break;
+				}
+			}
+		};
 		return binder;
 	}
 
+	@Override
+	public boolean onUnbind(Intent intent) {
+		Log.d(TAG, "onUnbind");
+		serviceHandler.getLooper().quit();
+		return false;
+	}
+
 	public void hello() {
-		serviceHandler.sendMessage(serviceHandler.obtainMessage(MSG_HELLO));
+		serviceHandler.sendMessage(serviceHandler.obtainMessage(MSG_HELLO, MSG_HELLO_OBJ));
 	}
 
 	public void bye() {
-		serviceHandler.sendMessage(serviceHandler.obtainMessage(MSG_BYE));
+		serviceHandler.sendMessage(serviceHandler.obtainMessage(MSG_BYE, MSG_BYE_OBJ));
 	}
 }
